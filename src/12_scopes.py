533c304ec36a0a0419d111d67b02fc4adc92e8a5
Mark Halls <mark.halls@gmail.com>
2020-03-10 16:07:04 -0700

    Learn about local scope, global scope and nonlocal scope

--- src/12_scopes.py
+++ src/12_scopes.py
@@ -4,8 +4,11 @@
 # When you use a variable in a function, it's local in scope to the function.
 x = 12
 
+
 def change_x():
+    global x
     x = 99
+
 
 change_x()
 
@@ -15,10 +18,13 @@ print(x)
 
 # This nested function has a similar problem.
 
+
 def outer():
     y = 120
 
     def inner():
+        # note, I really hate this from a functional programmers perspective
+        nonlocal y
         y = 999
 
     inner()
